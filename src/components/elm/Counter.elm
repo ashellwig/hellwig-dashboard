bb983931acf79590eefb1bae85332511b8ba4f51
Ashton Hellwig <hellwigashton@gmail.com>
2018-11-04 22:26:43 -0700

    add elm component

---
+++ src/components/elm/Counter.elm
@@ -0,0 +1,73 @@
+port module Main exposing (main)
+
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    Int
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+
+-- Update
+
+
+type Msg
+    = Increment
+    | Decrement
+    | Multiply Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Increment ->
+            ( model + 1, watchCounter (toString msg) )
+
+        Decrement ->
+            ( model - 1, watchCounter (toString msg) )
+
+        Multiply val ->
+            ( model * val, watchCounter (toString msg) )
+
+
+port counter : (Int -> msg) -> Sub msg
+
+
+port watchCounter : String -> Cmd msg
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    counter Multiply
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
